a682ad4ea806b627646b0d0bd6d50d89147b76ca
Madan.patakota <madan.patakota@gmail.com>
2025-12-24 20:30:18 +0530

    done

--- Nyayabharat.Api/Program.cs
+++ Nyayabharat.Api/Program.cs
@@ -1,75 +1,44 @@
 using Nyayabharat.Api.Extensions;
-using Nyayabharat.Api.Filters;
 using Nyayabharat.Api.Middlewares;
+using Nyayabharat.Api.Filters;
 
 var builder = WebApplication.CreateBuilder(args);
 
-/* -------------------- MVC -------------------- */
-
-bool jwtEnabled;
+/* ---------------- Controllers ---------------- */
 
 builder.Services.AddControllers(options =>
 {
-    // Validation can always run
     options.Filters.Add<ValidationFilter>();
-
-    // Auth-related filters ONLY if JWT is enabled
-    if (builder.Configuration.GetSection("Jwt").Exists())
-    {
-        options.Filters.Add<AuthorizationFilter>();
-        options.Filters.Add<AuditLogFilter>();
-    }
 });
 
-/* -------------------- SERVICES -------------------- */
+/* ---------------- Services ---------------- */
 
 builder.Services
     .AddApplicationServices(builder.Configuration)
     .AddSwaggerDocumentation();
 
-builder.Services.AddJwtAuthentication(builder.Configuration, out jwtEnabled);
-
-if (jwtEnabled)
-{
-    builder.Services.AddAuthorizationPolicies();
-}
-
 builder.Services.AddCorsPolicy();
 
-/* -------------------- APP -------------------- */
+/* ---------------- App ---------------- */
 
 var app = builder.Build();
-app.Logger.LogInformation("Nyayabharat API started successfully");
 
-
-/* -------------------- SWAGGER -------------------- */
+/* ---------------- Swagger ---------------- */
 
 app.UseSwagger();
 app.UseSwaggerUI();
-app.Logger.LogInformation("Nyayabharat API started successfully-Next");
 
+/* ---------------- Middleware ---------------- */
 
-/* -------------------- MIDDLEWARE -------------------- */
-
-// Custom middlewares (safe order)
 app.UseMiddleware<ExceptionMiddleware>();
 app.UseMiddleware<RequestLoggingMiddleware>();
 app.UseMiddleware<ResponseWrapperMiddleware>();
-
 
 app.UseCors("NyayabharatCors");
 
 app.UseHttpsRedirection();
 
-/* -------------------- AUTH -------------------- */
-
-if (jwtEnabled)
-{
-    app.UseAuthentication();
-    app.UseAuthorization();
-}
-
-/* -------------------- ENDPOINTS -------------------- */
+/* ---------------- Endpoints ---------------- */
 
 app.MapControllers();
 
