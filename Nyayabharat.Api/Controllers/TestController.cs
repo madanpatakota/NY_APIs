55c06477386b3eec783a319d21ac1ebb9655a7f6
Madan.patakota <madan.patakota@gmail.com>
2025-12-24 17:02:18 +0530

    test

---
+++ Nyayabharat.Api/Controllers/TestController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Nyayabharat.Api.Controllers
+{
+
+    [AllowAnonymous]
+    [ApiController]
+    [Route("api/[controller]")]
+    public class TestController: ControllerBase
+    {
+        [HttpGet]
+        [Route("GetName")]
+        public string GetName()
+        {
+            return "Madan";
+        }
+    }
+}
