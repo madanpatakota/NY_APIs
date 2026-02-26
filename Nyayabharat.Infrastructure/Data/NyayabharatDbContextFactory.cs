a9057dffea2167bc3fdabe891114bf2f83358fdc
Madan.patakota <madan.patakota@gmail.com>
2026-02-26 20:27:45 +0530

    done

--- Nyayabharat.Infrastructure/Data/NyayabharatDbContextFactory.cs
+++ Nyayabharat.Infrastructure/Data/NyayabharatDbContextFactory.cs
@@ -11,7 +11,10 @@ namespace Nyayabharat.Infrastructure.Data
             var optionsBuilder = new DbContextOptionsBuilder<NyayabharatDbContext>();
 
             optionsBuilder.UseSqlServer(
-                "Server=DESKTOP-1VJDAUH\\SQLEXPRESS01;Database=NyayabharatDB;Trusted_Connection=True;TrustServerCertificate=True;");
+                "Server=tcp:madny.database.windows.net,1433;Database=madny;User ID=madny;Password=Sahasra*06#;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;");
+
+            //optionsBuilder.UseSqlServer(
+            //    "Server=DESKTOP-1VJDAUH\\SQLEXPRESS01;Database=NyayabharatDB;Trusted_Connection=True;TrustServerCertificate=True;");
 
             return new NyayabharatDbContext(optionsBuilder.Options);
         }
