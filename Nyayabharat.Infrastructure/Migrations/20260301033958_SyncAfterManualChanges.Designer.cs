4f7ce94ac192eca869ccbd6dbc5fabf4de806aca
Madan.patakota <madan.patakota@gmail.com>
2026-03-01 09:23:01 +0530

    done

--- Nyayabharat.Infrastructure/Migrations/20260301033958_SyncAfterManualChanges.Designer.cs
+++ Nyayabharat.Infrastructure/Migrations/20260301033958_SyncAfterManualChanges.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Nyayabharat.Infrastructure.Data;
 
@@ -11,9 +12,11 @@ using Nyayabharat.Infrastructure.Data;
 namespace Nyayabharat.Infrastructure.Migrations
 {
     [DbContext(typeof(NyayabharatDbContext))]
-    partial class NyayabharatDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260301033958_SyncAfterManualChanges")]
+    partial class SyncAfterManualChanges
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
